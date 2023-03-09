337cd9e19c29d4bad59b19c3116be7655b39e379
crbel <crbelen28@gmail.com>
2023-03-09 18:25:08 +0100

    HundirLaFlota-0

---
+++ src/hundirlaflota/Hundirlaflota.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package hundirlaflota;
+
+/**
+ * @author crbel
+ *
+ */
+public class Hundirlaflota {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
